5801d3fbe756730712ea2561155f537e69244294
Doctor-wei <57200832+Doctor-wei@users.noreply.github.com>
2019-11-14 10:17:46 +0800

    模块化分解
    采用数组存储结构。
    划分为4个模块；
    1、生成算式
    2、检查重复
    3、打印算式
    4、打印结果

--- compute.java
+++ compute.java
@@ -1,19 +1,77 @@
 import java.util.Random;
 
 public class compute { 
-	public static void main(String[] args) {
-		int a=0,b=0,o=0;
-		char s='+';
-		Random random =new Random();
-		for(int i=0;i<50;i++) {
-			o=(int)random.nextInt(2);
-			a=(int)random.nextInt(101);
-			b=(int)random.nextInt(101);
-			if(o==1)
-				s='+';
-			else
-				s='-';
-			System.out.println(""+(i+1)+":\t"+a+s+b+"=");
+	static int Formula[][]= new int[51][3];//存储算式
+	static int Answer[] = new int[51];//存放答案
+	int i=0;
+	public  void generateEquations()//生成算式
+	{
+		int num1,num2,temp;
+	    int flag=0;
+		Random random=new Random();
+		while(i<50) {
+		num1=random.nextInt(101);//第一个数
+		num2=random.nextInt(101);//第二个数
+		temp=random.nextInt(2);//符号,1为加，2为减
+		flag=this.CheckRepeate(num1, num2, temp); //检查重复,1为重复
+		if(temp==1&&flag==0) {
+			if(num1+num2<=100) {
+				Formula[i][0]=num1;
+				Formula[i][1]=temp;
+				Formula[i][2]=num2;
+				Answer[i]=num1+num2;
+				i++;
+			}
 		}
+		else if(temp==0&&flag==0){
+			if(num1-num2>=0) {
+				Formula[i][0]=num1;
+				Formula[i][1]=temp;
+				Formula[i][2]=num2;
+				Answer[i]=num1-num2;
+				i++;
+			}
+		}
+		flag=1;
+		}
+	}	
+	public int CheckRepeate(int num1,int num2,int temp) {
+	    int flag1=0;
+		for(int j=0;j<i;j++) {
+			if(num1==Formula[j][0]&&temp==Formula[j][1]&&num2==Formula[j][2]) {
+				  flag1=1;
+				  break;
+			}
+		}
+		return flag1;//1为重复		
+	}
+	
+	public void printEquations() {	//输出算式
+		for(int i=0;i<50;i++) {
+			System.out.print("第"+(i+1)+"道题:"+Formula[i][0]+"");
+			if(Formula[i][1]==0)
+			System.out.print("-");
+			else
+			System.out.print("+");
+			System.out.print(Formula[i][2]+"=\t\t");
+			if((i+1)%5==0) {
+				System.out.println();
+			}
+		}
+	}
+	public void printAnswers() {	//输出答案
+		for(int i=0;i<50;i++) {
+			System.out.print("第"+(i+1)+"道题:"+Answer[i]+"     ");
+			if((i+1)%5==0) {
+				System.out.println();
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		compute a = new compute();
+		a.generateEquations();
+		a.printEquations();
+		a.printAnswers();
 	}
 }
